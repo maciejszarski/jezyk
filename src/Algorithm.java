630d661cda5840816343c450994a78a6ce250dad
maciejszarski <macieksz90@gmail.com>
2015-02-10 00:04:38 +0100

    Lematyzacja

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -5,8 +5,15 @@ import java.io.FileReader;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Hashtable;
+import java.util.List;
+
+import org.annolab.tt4j.TokenHandler;
+import org.annolab.tt4j.TreeTaggerException;
+import org.annolab.tt4j.TreeTaggerWrapper;
 
 public class Algorithm {
 	private final String[] ignoreFiles = new String[]{".DS_Store"};
@@ -15,11 +22,14 @@ public class Algorithm {
 	private String sentencesPath = "sentences";
 	private String berkeleyAlignerPath = "berkeleyaligner";
 	private String alignedPath = "aligned";
+	private String taggerPath = "treetagger";
 	private FilenameFilter filenameFilter = new FilenameFilter() {
 	    public boolean accept(File folder, String name) {
 	        return !Arrays.asList(ignoreFiles).contains(name);
 	    }
 	};
+	String[] lemmas;
+	int lemmasIndex;
 		
 	public Algorithm(){
 		filesPathEN = Algorithm.class.getResource(filesPathEN).getPath().replaceAll("%20"," ");
@@ -27,6 +37,7 @@ public class Algorithm {
 		sentencesPath = Algorithm.class.getResource(sentencesPath).getPath().replaceAll("%20"," ");
 		berkeleyAlignerPath = Algorithm.class.getResource(berkeleyAlignerPath).getPath().replaceAll("%20"," ");
 		alignedPath = Algorithm.class.getResource(alignedPath).getPath().replaceAll("%20"," ");
+		taggerPath = Algorithm.class.getResource(taggerPath).getPath().replaceAll("%20"," ");
 	}
 	
 	public void alignSentences() throws Exception{
@@ -120,27 +131,50 @@ public class Algorithm {
 		}	
 	}
 	
-	public Hashtable<String,Hashtable<String,Integer>> getTranslations(){
+	public List<String[]> generateLemmats(String alignedFilePath, String modelPath){
+		System.out.println("Generating lemmats...");
+		List<String[]> result = new ArrayList<String[]>();
+		try {
+			File file = new File(alignedPath+"/"+alignedFilePath);
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String line;
+			List<String> words = new ArrayList<String>();
+			List<Integer> lengths = new ArrayList<Integer>();
+			while ((line = br.readLine()) != null) {
+				String[] splitted = line.split("[., :;!?]+");
+				words.addAll(Arrays.asList(splitted));
+				lengths.add(splitted.length);
+			}
+			br.close();
+			String[] lemmats = lemmatize(words,modelPath);
+			int i = 0;
+			while(lemmats.length>0){
+				String[] lemmatsLine = Arrays.copyOfRange(lemmats, 0, lengths.get(i));
+				result.add(lemmatsLine);
+				lemmats = Arrays.copyOfRange(lemmats, lengths.get(i), lemmats.length);
+				i++;
+			}
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	public Hashtable<String,Hashtable<String,Integer>> getTranslations(List<String[]> lemmatsEN, List<String[]> lemmatsPL){
+		System.out.println("Preparing translations from aligned files...");
 		Hashtable<String,Hashtable<String,Integer>> translations = new Hashtable<String,Hashtable<String,Integer>>();
 		try {
 			File alignFile = new File(alignedPath+"/training.align");
-			File enFile = new File(alignedPath+"/training.en");
-			File plFile = new File(alignedPath+"/training.pl");
 			BufferedReader alignBR = new BufferedReader(new FileReader(alignFile));
-			BufferedReader enBR = new BufferedReader(new FileReader(enFile));
-			BufferedReader plBR = new BufferedReader(new FileReader(plFile));
-			String alignLine, enLine, plLine;
+			String alignLine;
+			int i = 0;
 			while ((alignLine = alignBR.readLine()) != null) {
-			   enLine = enBR.readLine();
-			   plLine = plBR.readLine();	
-//			   System.out.println(enLine);
-//			   System.out.println(plLine);
 			   String[] aligns = alignLine.split(" ");
-			   String[] enWords = enLine.split(" ");
-			   String[] plWords = plLine.split(" ");
-			   for(int i=0; i<aligns.length; i++){
-				   //System.out.println(i+": "+aligns[i]);
-				   String[] pair = aligns[i].split("-");
+			   String[] enWords = lemmatsEN.get(i);
+			   String[] plWords = lemmatsPL.get(i);
+			   for(int j=0; j<aligns.length; j++){
+				   String[] pair = aligns[j].split("-");
 				   String plWord = plWords[Integer.parseInt(pair[0])];
 				   String enWord = enWords[Integer.parseInt(pair[1])];
 				   if(translations.containsKey(enWord)){
@@ -156,10 +190,9 @@ public class Algorithm {
 					   translations.put(enWord, translation);
 				   }
 			   }
+			   i++;
 			}
 			alignBR.close();
-			enBR.close();
-			plBR.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -168,6 +201,7 @@ public class Algorithm {
 	}
 	
 	public Hashtable<String,String> createDictionary(Hashtable<String,Hashtable<String,Integer>> translations){
+		System.out.println("Creating dictionary...");
 		Hashtable<String,String> dictionary = new Hashtable<String,String>();
 		for (String enWord : translations.keySet()) {
 			int maxTranslations = 0;
@@ -184,8 +218,39 @@ public class Algorithm {
 	}
 	
 	public void printDictionary(Hashtable<String,String> dictionary){
+		System.out.println("Result:");
 		for (String word : dictionary.keySet()) {
 			System.out.println(word + " - " + dictionary.get(word));
 		}
+	}
+	
+	public String[] lemmatize(List<String> words, String modelPath){
+		long timeStart = Calendar.getInstance().getTimeInMillis();
+		//more doc on the tagger at https://code.google.com/p/tt4j/
+		lemmas = new String[words.size()];
+		lemmasIndex = 0;
+		System.setProperty("treetagger.home", taggerPath);
+	    TreeTaggerWrapper<String> tt = new TreeTaggerWrapper<String>();
+	    try {
+	    	tt.setModel(modelPath);
+	    	tt.setHandler(new TokenHandler<String>() {
+		        public void token(String token, String pos, String lemma) {
+		        	System.out.println(token + "\t" + pos + "\t" + lemma);
+		        	lemmas[lemmasIndex] = lemma;
+		        	lemmasIndex++;
+		        }
+		    });
+    		tt.process(words);
+	    } catch(IOException e){
+	    	e.printStackTrace();
+	    } catch(TreeTaggerException e) {
+			e.printStackTrace();
+		}
+	    finally {
+	    	tt.destroy();
+	    }
+	    long timeEnd = Calendar.getInstance().getTimeInMillis();
+		System.out.println("Total time: "+(timeEnd-timeStart));
+	    return lemmas;
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,20 @@ import java.util.Hashtable;
 import java.util.List;
 
 public class Main {
+	private final static String modelPathEN = "en.bin";
+	private final static String modelPathPL = "pl.bin";
+	private final static String alignedFileEN = "training.en";
+	private final static String alignedFilePL = "training.pl";
 
 	public static void main(String[] args){
 		Algorithm algorithm = new Algorithm();
 		try{
-			algorithm.alignSentences();
-			algorithm.generateConfigFile();
-			algorithm.alignWords();
-			Hashtable<String,Hashtable<String,Integer>> translations = algorithm.getTranslations();
+//			algorithm.alignSentences();
+//			algorithm.generateConfigFile();
+//			algorithm.alignWords();
+			List<String[]> lemmatsEN = algorithm.generateLemmats(alignedFileEN,modelPathEN);
+			List<String[]> lemmatsPL = algorithm.generateLemmats(alignedFilePL,modelPathPL);
+			Hashtable<String,Hashtable<String,Integer>> translations = algorithm.getTranslations(lemmatsEN,lemmatsPL);
 			Hashtable<String,String> dictionary = algorithm.createDictionary(translations);
 			algorithm.printDictionary(dictionary);
 			System.out.println("Done.");
